799552728bbc43f035f9485c8035132fa2f3aee3
2814109 <2814109k@gmail.com>
2025-01-01 21:27:23 +0900

    chore

---
+++ error-symbol.ts
@@ -0,0 +1,21 @@
+const BrandTypeId: unique symbol = Symbol.for("effect/Brand")
+
+type ProductId = number & {
+  readonly [BrandTypeId]: {
+    readonly ProductId: "ProductId"
+  }
+}
+
+const getProductById = (id: ProductId) => {
+  // Logic to retrieve product
+}
+
+type UserId = number
+
+const id: UserId = 1
+
+getProductById(id)
+/*
+Argument of type 'number' is not assignable to parameter of type 'ProductId'.
+  Type 'number' is not assignable to type '{ readonly [BrandTypeId]: { readonly ProductId: "ProductId"; }; }'.ts(2345)
+*/
